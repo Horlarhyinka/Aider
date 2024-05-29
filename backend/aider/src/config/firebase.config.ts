510203ab454ca0412d72b06757f66df8304bf692
Horlarhyinka <danijufarouq2003@gmail.com>
2024-05-29 10:35:33 +0100

    backend: setup server side logic for push notification

--- backend/aider/src/config/firebase.config.ts
+++ backend/aider/src/config/firebase.config.ts
@@ -1,7 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "./config";
 import { getFirestore } from "firebase/firestore";
-
+import firebase from "firebase/compat/app";
+import admin from "firebase-admin"
+import path from "path"
 
 export async function getFirestoreDB (){
     try{
@@ -11,4 +13,16 @@ export async function getFirestoreDB (){
         console.log(err)
         process.exit(1)
     }
+}
+
+export async function getFirebaseAdmin(){
+
+var serviceAccount = require(path.resolve(__dirname, "../../fb-service-account.json"));
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+return admin
+
 }
